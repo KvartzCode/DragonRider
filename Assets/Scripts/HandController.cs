1d76cc7783b04c0e5fc6a6af6adc37ccb57851e2
KvartzCode <113012348+KvartzCode@users.noreply.github.com>
2023-10-03 14:53:50 +0200

    Fixed reset rotation

--- Assets/Scripts/HandController.cs
+++ Assets/Scripts/HandController.cs
@@ -13,6 +13,8 @@ public class HandController : MonoBehaviour
     [SerializeField] Mesh normalHand;
     [SerializeField] Mesh grabbingHand;
 
+    [SerializeField] DragonController dragonController;
+
     private Vector3 originalPosition;
     private Quaternion originalRotation;
 
@@ -28,6 +30,9 @@ public class HandController : MonoBehaviour
 
         originalPosition = handMesh.transform.localPosition;
         originalRotation = handMesh.transform.localRotation;
+
+        if (!dragonController)
+            dragonController = gameObject.transform.root.GetComponent<DragonController>();
     }
 
     
@@ -38,6 +43,8 @@ public class HandController : MonoBehaviour
         handParent.transform.localPosition = Vector3.zero;
         handMesh.transform.localPosition = new Vector3(0.0449f, -0.0056f, -0.0004f);
         handMesh.transform.localRotation = new Quaternion(-0.0905127451f, 0.0703139678f, -0.684613943f, 0.719838321f); //magic numbers, DO NOT TOUCH!
+
+        dragonController.StopTweenRotation();
     }
 
     public void HandReleased()
@@ -48,5 +55,7 @@ public class HandController : MonoBehaviour
         handParent.transform.localRotation = new Quaternion();
         handMesh.transform.localPosition = originalPosition;
         handMesh.transform.localRotation = originalRotation;
+
+        dragonController.ResetRotation();
     }
 }
