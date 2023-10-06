2334ec6ef6663141175fa9c0e045792d983f5d2b
KvartzCode <113012348+KvartzCode@users.noreply.github.com>
2023-10-06 09:12:48 +0200

    Added game timer

--- Assets/Scripts/GameTimer.cs
+++ Assets/Scripts/GameTimer.cs
@@ -6,11 +6,22 @@ public class GameTimer : MonoBehaviour
 {
     public float Timer { get; private set; } = 300;
 
+    private int extraTime = 300;
+
+    [SerializeField] TimerDisplay timerDisplay;
+
     //public float Timer = 20; //For testing
 
     void Update()
     {
         Timer -= Time.deltaTime;
+
+        if (extraTime - Timer > 1)
+        {
+            extraTime = (int)Timer;
+            timerDisplay.SetTime(extraTime);
+        }
+
         if (Timer <= 0)
             EndGame();
     }
